ba3a2c52dc4e2a343fb701671c04ff3cce9727d4
Milind Patel <milindpatel63@gmail.com>
2022-11-22 05:39:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
 	const GenerateQRCode = () => {
 	const guestpass=process.env.GUESTPASS;
+	console.log("GPASS: "+guestpass)
 		const url1 = "WIFI:S:Diana Network Guest1;T:WPA2;P:"+guestpass+";;"
 		QRCode.toDataURL(url1, {
 			width: 800,
