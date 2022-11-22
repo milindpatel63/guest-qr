00b277f6f60688f748601bb51e62d19ea744fd5f
Milind Patel <milindpatel63@gmail.com>
2022-11-22 08:05:46 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import QRCode from 'qrcode'
 import { useState,useEffect } from 'react'
-import './App.css'
+//import './App.css'
 import gpass from './guest.js'
 function App() {
-	const [url, setUrl] = useState('')
 	const [qr, setQr] = useState('')
 
-	const GenerateQRCode = () => {
-	}
 	useEffect(() => {
 	console.log("GPASS1: "+gpass)
-	const url1 = "WIFI:S:Diana Network Guest1;T:WPA2;P:"+gpass+";;"
-		QRCode.toDataURL(url1, {
+	const url = "WIFI:S:Diana Network Guest1;T:WPA2;P:"+gpass+";;"
+		QRCode.toDataURL(url, {
 			width: 800,
 			margin: 2,
 			color: {
 				dark: '#335383FF',
 				light: '#EEEEEEFF'
 			}
-		}, (err, url1) => {
+		}, (err, url) => {
 			if (err) return console.error(err)
 
-			console.log(url1)
-			setQr(url1)
+			console.log(url)
+			setQr(url)
 		})
 	},[]);
 
 	return (
 		<div className="app">
-			<h1>QR Generator</h1>
-			<input 
-				type="text"
-				placeholder="e.g. https://google.com"
-				value={url}
-				onChange={e => setUrl(e.target.value)} />
-			<button onload={GenerateQRCode}>Generate</button>
+			<h3>Welcome to,</h3>
+			<h1>Diana Network</h1>
+			<p>SSID: <b>Diana Network Guest</b></p>
+			<p>PASSWORD: <b>{gpass}</b></p>
 			{qr && <>
 				<img alt="qr code" src={qr} />
 				<a href={qr} download="qrcode.png">Download</a>
